2eedd2e30cb3213dae93f1f109a5ccea6ae1291d
Bhavish2 <85808960+Bhavish2@users.noreply.github.com>
2021-11-08 02:50:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -220,7 +220,7 @@ if(process.env.NODE_ENV==="production")
 {
  // app.use(express.static("my-app/public"));
   const path=require("path");
-  app.get("/*",(req,res)=>{
+  app.get("*",(req,res)=>{
     res.sendFile(path.resolve(__dirname,'my-app','build','index.html'));
   })
 
